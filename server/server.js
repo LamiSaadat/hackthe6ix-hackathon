f760cf238e61841e32e40b53ba820657776988df
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-08-20 17:44:38 -0400

    Updated server port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ dotenv.config({ path: "./.env" });
 const app = require("./app");
 
 // LISTEN ON PORT
-const port = process.env.PORT || 80800;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Listening on port: ${port} 🚀🚀`);
 });
