94564c247c8c7d77cc7ca5c917d95cee9f7c558a
Nicole Cevey <nicole.cevey@gmail.com>
2022-08-20 13:43:07 -0400

    Adding initial styling for navbar

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import "./Navigation.scss";
 import logo from "../../assets/images/wealthsimple-logo.png";
 import { Link } from "react-router-dom";
+import downArrow from "../../assets/icons/down-arrow.png";
 
 function Navigation() {
   return (
@@ -8,6 +9,24 @@ function Navigation() {
       <Link to="https://www.wealthsimple.com/">
         <img src={logo} className="nav__logo"></img>
       </Link>
+      <ul className="nav__list">
+        <li className="nav__list-item">
+          Invest
+          <img src={downArrow} alt="Down arrow" className="nav__icon"></img>
+        </li>
+        <Link to="https://www.wealthsimple.com/en-ca/spend">Spend & save</Link>
+        <Link to="https://www.wealthsimple.com/en-ca/tax">Tax</Link>
+        <li className="nav__list-item">
+          Learn
+          <img src={downArrow} className="nav__icon" alt="Down arrow"></img>
+        </li>
+      </ul>
+      <div className="nav__actions">
+        <Link to="https://my.wealthsimple.com/app/public/signup?locale=en-ca">
+          Log in
+        </Link>
+        <button className="nav__button">Download the app</button>
+      </div>
     </nav>
   );
 }
